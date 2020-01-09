f08d45b1f5cd20a9f48db820552fe7644be3174b
bbaappss <email@brandonandrewscott.com>
2020-01-09 00:41:11 -0800

    Landing page first past

--- src/js/site/main.js
+++ src/js/site/main.js
@@ -1,12 +1,1 @@
-(function() {
-
-  var menuTrigger = Trigger.init({
-    trigger: '.menu-trigger',
-    bodyClass: 'menu-activated'
-  });
-
-  Lantern.init();
-
-  Sail.init();
-
-})();
+AOS.init();
