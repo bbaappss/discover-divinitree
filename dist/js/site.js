62c067f36f67fe64c7ebfa6ae0ae3bfd74c9ee3b
bbaappss <email@brandonandrewscott.com>
2020-01-19 12:35:16 -0700

    Incorporate Esme's and Jill's copy updates

--- dist/js/site.js
+++ dist/js/site.js
@@ -606,6 +606,10 @@ var Lantern = (function() {
 
 AOS.init();
 
-document.querySelector("#click48mindbodylink").addEventListener('click', function () {
+var x, i;
+x = document.querySelectorAll(".click48mindbodylink");
+for (i = 0; i < x.length; i++) {
+  x[i].addEventListener('click', function () {
     document.querySelector("#healCode48container a").click();
-});
+  });
+}
